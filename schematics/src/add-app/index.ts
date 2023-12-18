60dfa9e5e215f01302ad65fbfec420ccb28079f1
Grzegorz Wadas <grzegorzwadas@codegate.pl>
2023-12-18 22:51:44 +0100

    feat: added inquirer

--- schematics/src/add-app/index.ts
+++ schematics/src/add-app/index.ts
@@ -8,16 +8,12 @@ import {
   strings,
   chain,
   mergeWith,
-  move
+  move,
+  SchematicsException
 } from '@angular-devkit/schematics'
 import { parse, stringify } from 'yaml'
-
-type AddAppSchematicOptions = {
-  appName: string,
-  imageURL: string,
-  projectName: string,
-  appPort: number
-}
+import { input } from '@inquirer/prompts'
+import select from '@inquirer/select'
 
 const updateKustomization = (projectName: string, appName: string): Rule => {
   return (tree: Tree, _: SchematicContext): Tree => {
@@ -25,7 +21,7 @@ const updateKustomization = (projectName: string, appName: string): Rule => {
     const file = tree.read(path)?.toString()
 
     if (!file) {
-      throw new Error('missing file')
+        throw new SchematicsException('missing file')
     }
 
     const yaml = parse(file)
@@ -44,31 +40,47 @@ const updateKustomization = (projectName: string, appName: string): Rule => {
   }
 }
 
-export const add = (options: AddAppSchematicOptions): Rule => {
-  return (_tree: Tree, context: SchematicContext) => {
+export const add = (): Rule => {
+  return async (tree: Tree) => {
     const projectConfigPath = `./argo-composer.config.yaml`
-    const file = _tree.read(projectConfigPath)?.toString()
+    const file = tree.read(projectConfigPath)?.toString()
 
     if (!file) {
-      throw new Error('no initialized project! Please start from init command!')
+      throw new SchematicsException('no initialized project! Please start from init command!')
     }
 
     const config = parse(file) //todo: add global type for project config and pass whole object
     const mainProjectName = config.name
     const mainRepoURL = config.repoUrl
 
+    const currentProjects = tree.getDir('projects').subdirs
+
+    const appName = await input({ message: 'What name would you like to use for the app?' })
+    const imageURL = await input({ message: 'Image URL, example: your-registry.com/your-app' })
+    const projectName = await select({
+      message: 'Please select project where you want add your app',
+      choices: currentProjects.map(currentProject => ({
+        name: currentProject.toString(),
+        value: currentProject.toString(),
+      }))
+    }) as string
+    const appPort = await input({ message: 'Provide port of the app' })
+
+    const options = {
+      appName,
+      imageURL,
+      projectName,
+      appPort
+    }
+
     const templateSource = apply(url('./files'), [
       template({ ...options, ...strings, mainProjectName, mainRepoURL }),
       move(`/projects/${options.projectName}/apps/`)
     ])
 
-    const merged = mergeWith(templateSource)
-
-    const rule = chain([
-      merged,
+    return chain([
+      mergeWith(templateSource),
       updateKustomization(options.projectName, options.appName)
     ])
-
-    return rule(_tree, context) as Rule
   }
 }

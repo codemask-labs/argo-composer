5dd7b83ff38a54a4883f8ad98258c2146a496ce6
Grzegorz Wadas <grzegorzwadas@codegate.pl>
2024-02-19 14:34:05 +0100

    feat: removed schematics from remove app command

--- commands/remove-app.command.ts
+++ commands/remove-app.command.ts
@@ -1,9 +1,4 @@
 import { program } from 'commander'
 import { removeAppAction } from '../actions'
 
-export const removeAppCommand = () =>
-    program
-        .command('remove-app')
-        .alias('ra')
-        .description('Remove app from project')
-        .action(removeAppAction)
+export const removeAppCommand = () => program.command('remove-app').alias('ra').description('Remove app from project').action(removeAppAction)

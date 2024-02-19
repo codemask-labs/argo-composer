c0d37467b7cbfc314f2f3977f13079b61b8d8ef7
Grzegorz Wadas <grzegorzwadas@codegate.pl>
2024-02-19 14:24:56 +0100

    chore: format

--- commands/remove-project.command.ts
+++ commands/remove-project.command.ts
@@ -2,8 +2,4 @@ import { program } from 'commander'
 import { removeProjectAction } from '../actions'
 
 export const removeProjectCommand = () =>
-    program
-        .command('remove-project')
-        .alias('rp')
-        .description('Remove project with applications')
-        .action(removeProjectAction)
+    program.command('remove-project').alias('rp').description('Remove project with applications').action(removeProjectAction)

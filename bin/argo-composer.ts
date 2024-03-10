692bd67223d334bd24cf1dc47f95ced8a234f99e
Przemyslaw Walczak (Windows) <p.d.walczak@gmail.com>
2024-03-10 20:56:26 +0100

    feat: added node-cli-toolkit

--- bin/argo-composer.ts
+++ bin/argo-composer.ts
@@ -1,10 +1,73 @@
-#!/usr/bin/env node
-import { program } from 'commander'
-import { runtime } from '../runtime'
-import { commandLoader } from '../commands'
+import { StacklessError, command, commands, getPackageJson, program, usage, version } from '@codemaskjs/node-cli-toolkit'
+import { addProjectAction, initProjectAction, removeAppAction, removeProjectAction } from '../actions'
 
-program.version(runtime.package.version, '-v, --version', 'Output the current version.')
+const { version: programVersion } = getPackageJson()
 
-commandLoader()
+program([
+    version(programVersion),
+    usage('argo-composer'),
+    commands([
+        command('init', {
+            short: 'i',
+            description: 'Initializes argo composer root directory',
+            next: () => initProjectAction()
+        }),
+        command('add <resource>', {
+            description: 'Adds new resource',
+            next: async ({ resource }) => {
+                switch (resource) {
+                    case 'app':
+                    case 'application':
+                        throw new Error('not yet supported')
 
-program.parse(process.argv)
+                    case 'project':
+                        await addProjectAction()
+                        break
+
+                    default:
+                        throw new StacklessError(`Adding resource '${resource}' is not supported. Allowed: 'app', 'application' or 'project'.`)
+                }
+            }
+        }),
+        command('remove <resource>', {
+            short: 'rm',
+            description: 'Removes a resource',
+            next: async ({ resource }) => {
+                switch (resource) {
+                    case 'app':
+                    case 'application':
+                        await removeAppAction()
+                        break
+
+                    case 'project':
+                        await removeProjectAction()
+                        break
+
+                    default:
+                        throw new StacklessError(`Removing resource '${resource}' is not supported. Allowed: 'app', 'application' or 'project'.`)
+                }
+            }
+        })
+        // todo: implement the below commands
+        // command('move <resource>', {
+        //     short: 'mv',
+        //     description: 'Moves a resource from project to another project',
+        //     next: params => {}
+        // }),
+        // command('copy <resource>', {
+        //     short: 'cp',
+        //     description: 'Copies a resource from project to another project',
+        //     next: params => {}
+        // }),
+        // command('rename <resource>', {
+        //     description: 'Renames a resource',
+        //     next: params => {
+        //         console.log('Hello debug!', params)
+        //     }
+        // }),
+        // command('apply <resource>', {
+        //     description: 'Applies changes including `.overrides` to selected resources',
+        //     next: params => {}
+        // })
+    ])
+])

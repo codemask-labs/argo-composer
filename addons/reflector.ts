f1401d514e825b537e3766c4f95e653c673a8dc7
Grzegorz Wadas <grzegorzwadas@codegate.pl>
2024-06-19 19:53:22 +0200

    fix: added annotations for addons

--- addons/reflector.ts
+++ addons/reflector.ts
@@ -6,7 +6,10 @@ const application: Application = {
     kind: 'Application',
     metadata: {
         name: 'reflector',
-        namespace: 'argocd'
+        namespace: 'argocd',
+        annotations: {
+            'argocd.argoproj.io/sync-wave': '-10'
+        }
     },
     spec: {
         project: 'default',

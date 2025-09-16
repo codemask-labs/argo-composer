9165620c9d7b89234b3a86e2505ac3b0c91e1a6f
Przemyslaw Walczak <p.d.walczak@gmail.com>
2025-09-16 10:19:17 +0200

    feat: reading project list interractively

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use cliclack::{intro, select};
 use theme::*;
 use utils::get_project_list;
 
-use crate::utils::get_composer_config;
+use crate::utils::get_argo_composer_config;
 
 const ARGO_COMPOSER_NOT_INITIALIZED: &str = "Argo composer not initialized\nPlease make sure you have initialized the project with `argo-composer init` command.";
 
@@ -40,7 +40,7 @@ struct ArgoComposer {
 
 fn main() {
     let cli = ArgoComposer::parse();
-    let composer_config = get_composer_config();
+    let composer_config = get_argo_composer_config();
 
     match cli.command.unwrap() {
         Commands::Init => {
@@ -66,20 +66,32 @@ fn main() {
                 }
 
                 let composer_config = composer_config.unwrap();
-                let project_list = get_project_list("./example/projects");
+                let project_dir = composer_config.root_directory.join("projects");
+                let project_list = get_project_list(project_dir.to_str().unwrap());
+                let project_items: Vec<(String, String, String)> = project_list
+                    .into_iter()
+                    .map(|project| (project.name.clone(), project.name.clone(), String::new()))
+                    .collect();
 
-                println!("Root directory: {:?}", composer_config.root_directory);
-                println!("Projects: {:#?}", project_list);
+                if project_items.is_empty() {
+                    terminate_with_message(
+                        "No projects found\nPlease add a project first with `argo-composer add project` command.",
+                    );
+                }
 
-                let project = select("Select a project")
-                    .items(&[
-                        ("project-1", "Project 1", "Project 1 description"),
-                        ("project-2", "Project 2", "Project 2 description"),
-                    ])
+                let selected_project = select("Select a project")
+                    .items(project_items.as_slice())
                     .interact()
                     .unwrap();
 
-                println!("Selected project: {}", project);
+                let project_apps = project_dir.join(&selected_project).join("apps");
+                let project_presets = project_dir.join(&selected_project).join(".presets");
+
+                // let selected_preset =
+                //     select("Select application preset").items(application_presets);
+
+                println!("Apps dir: {}", project_apps.to_str().unwrap());
+                println!("Presets dir: {}", project_presets.to_str().unwrap());
             }
         },
         Commands::Remove { command } => match command {

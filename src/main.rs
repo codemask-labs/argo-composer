aa323c563c7a3618a4c790a88a02188ede27695c
Przemyslaw Walczak <p.d.walczak@gmail.com>
2025-09-16 20:30:49 +0200

    feat: preset input placeholder

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod theme;
 mod utils;
 
 use clap::{Parser, Subcommand};
-use cliclack::{intro, select};
+use cliclack::{input, intro, select};
 use theme::*;
 use utils::get_project_list;
 
-use crate::utils::get_argo_composer_config;
+use crate::utils::{get_argo_composer_config, get_project_presets};
 
 const ARGO_COMPOSER_NOT_INITIALIZED: &str = "Argo composer not initialized\nPlease make sure you have initialized the project with `argo-composer init` command.";
 
@@ -52,11 +52,49 @@ fn main() {
         }
         Commands::Add { command } => match command {
             Resources::Project => {
-                intro("Adding project").unwrap();
+                intro("Adding new project").unwrap();
 
                 if composer_config.is_none() {
                     terminate_with_message(ARGO_COMPOSER_NOT_INITIALIZED);
                 }
+
+                let composer_config = composer_config.unwrap();
+                let project_dir = composer_config.root_directory.join("projects");
+                let project_dir_clone = composer_config.root_directory.join("projects");
+                let project_name: String = input("Project name")
+                    .validate_on_enter(move |name: &String| {
+                        if name.is_empty() {
+                            return Err("Project name cannot be empty".to_string());
+                        }
+
+                        if !name.chars().all(|c| c.is_ascii_lowercase() || c == '-') {
+                            return Err(
+                                "Project name can only contain lowercase letters and hyphens"
+                                    .to_string(),
+                            );
+                        }
+
+                        if name.starts_with('-') || name.ends_with('-') {
+                            return Err("Project name cannot start or end with hyphens".to_string());
+                        }
+
+                        if !name.split('-').all(|part| !part.is_empty()) {
+                            return Err(
+                                "Project name can only have single hyphen in between the words"
+                                    .to_string(),
+                            );
+                        }
+
+                        if project_dir_clone.join(name).exists() {
+                            return Err("Project already exists".to_string());
+                        }
+
+                        Ok(())
+                    })
+                    .interact()
+                    .unwrap();
+
+                println!("Adding project in: {:?}", project_dir.join(&project_name));
             }
             Resources::Application => {
                 intro("Adding application to a existing project").unwrap();
@@ -86,12 +124,26 @@ fn main() {
 
                 let project_apps = project_dir.join(&selected_project).join("apps");
                 let project_presets = project_dir.join(&selected_project).join(".presets");
+                let project_presets = get_project_presets(project_presets.to_str().unwrap());
 
-                // let selected_preset =
-                //     select("Select application preset").items(application_presets);
+                let selected_preset = select("Select a preset")
+                    .items(
+                        project_presets
+                            .iter()
+                            .map(|(preset_name, preset)| {
+                                (preset.clone(), preset_name.clone(), String::new())
+                            })
+                            .collect::<Vec<_>>()
+                            .as_slice(),
+                    )
+                    .interact()
+                    .unwrap();
 
-                println!("Apps dir: {}", project_apps.to_str().unwrap());
-                println!("Presets dir: {}", project_presets.to_str().unwrap());
+                println!("Selected project: {}", selected_project);
+                println!("Selected preset: {:?}", selected_preset);
+
+                // println!("Apps dir: {}", project_apps.to_str().unwrap());
+                // println!("Presets dir: {}", project_presets.to_str().unwrap());
             }
         },
         Commands::Remove { command } => match command {
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     env::current_dir,
     io::Read,
     path::{Path, PathBuf},
@@ -58,4 +59,85 @@ pub fn get_argo_composer_config() -> Option<ArgoComposerConfig> {
         }
         Err(_) => None,
     }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PresetInputType {
+    String,
+    Number,
+    Boolean,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct PresetInput {
+    ty: PresetInputType,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ProjectPreset {
+    pub name: String,
+    pub description: Option<String>,
+    pub template_directory: PathBuf,
+    pub inputs: Vec<PresetInput>,
+}
+
+pub fn get_project_presets(project_presets_dir: &str) -> HashMap<String, ProjectPreset> {
+    let cwd = current_dir().unwrap();
+    let profile_presets_dir = Path::new(&cwd)
+        .join(".argo-composer")
+        .join("default")
+        .join("presets");
+    let profile_presets = std::fs::read_dir(profile_presets_dir).unwrap();
+    let project_presets = std::fs::read_dir(project_presets_dir).unwrap();
+
+    let mut result = HashMap::new();
+
+    let presets = profile_presets
+        .into_iter()
+        .chain(project_presets.into_iter());
+
+    for preset in presets.into_iter() {
+        let preset_path = preset.unwrap().path();
+
+        if !preset_path.is_dir() {
+            continue;
+        }
+
+        let preset_config_path = preset_path.join("preset.yaml");
+        let preset_config_file = std::fs::File::open(preset_config_path);
+
+        if preset_config_file.is_err() {
+            continue;
+        }
+
+        let mut preset_config_contents = String::new();
+
+        if let Err(_) = preset_config_file
+            .unwrap()
+            .read_to_string(&mut preset_config_contents)
+        {
+            continue;
+        }
+
+        let preset_config_docs = YamlLoader::load_from_str(&preset_config_contents);
+
+        if preset_config_docs.is_err() {
+            continue;
+        }
+
+        let docs = preset_config_docs.unwrap();
+        let name = docs[0]["name"].as_str().unwrap().to_string();
+
+        result.insert(
+            name.clone(),
+            ProjectPreset {
+                name,
+                description: docs[0]["description"].as_str().map(|s| s.to_string()),
+                template_directory: preset_path.join("template"),
+                inputs: Vec::new(),
+            },
+        );
+    }
+
+    result
 }
